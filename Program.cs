fd3ca87fa5fe59003b434a2cd2d0592bceb1da23
ferh0162 <ferh0162@stud.kea.dk>
2023-09-13 11:52:55 +0200

    removed rules

--- Program.cs
+++ Program.cs
@@ -13,82 +13,51 @@ class Program
         List<List<string>> rounds = LoadRounds(); // Load round data into a list of lists.
 
         // Process each round to update club statistics.
-        for (int roundNumber = 0; roundNumber < rounds.Count; roundNumber++)
+        foreach (var round in rounds)
         {
-            List<string> round = rounds[roundNumber];
-
-            // Rule 6: Present upper and lower fraction tables separately after the split.
-            if (roundNumber == 22)
-            {
-                Console.WriteLine("Upper Fraction Table:");
-                DisplayTable(clubs.Where(c => c.SpecialRanking == 'U').ToList());
-                Console.WriteLine("\nLower Fraction Table:");
-                DisplayTable(clubs.Where(c => c.SpecialRanking == 'L').ToList());
-            }
-
-            // Process each match in the round.
             foreach (var match in round)
             {
-                // Rule 1: Error handling
-                try
+                // Split the match data into components.
+                var matchData = match.Split(',');
+                string homeTeamAbbreviation = matchData[0];
+                string awayTeamAbbreviation = matchData[1];
+                int homeGoals = int.Parse(matchData[2].Split('-')[0]);
+                int awayGoals = int.Parse(matchData[2].Split('-')[1]);
+
+                // Find the corresponding home and away clubs.
+                var homeClub = clubs.First(c => c.Abbreviation == homeTeamAbbreviation);
+                var awayClub = clubs.First(c => c.Abbreviation == awayTeamAbbreviation);
+
+                // Update club statistics based on match result.
+                homeClub.GamesPlayed++;
+                awayClub.GamesPlayed++;
+
+                homeClub.GoalsFor += homeGoals;
+                homeClub.GoalsAgainst += awayGoals;
+                awayClub.GoalsFor += awayGoals;
+                awayClub.GoalsAgainst += homeGoals;
+
+                if (homeGoals > awayGoals)
                 {
-                    var matchData = match.Split(',');
-                    string homeTeamAbbreviation = matchData[0];
-                    string awayTeamAbbreviation = matchData[1];
-                    int homeGoals = int.Parse(matchData[2].Split('-')[0]);
-                    int awayGoals = int.Parse(matchData[2].Split('-')[1]);
-
-                    // Rule 2: Only process known teams from the teams file.
-                    if (clubs.Any(c => c.Abbreviation == homeTeamAbbreviation) &&
-                        clubs.Any(c => c.Abbreviation == awayTeamAbbreviation))
-                    {
-                        var homeClub = clubs.First(c => c.Abbreviation == homeTeamAbbreviation);
-                        var awayClub = clubs.First(c => c.Abbreviation == awayTeamAbbreviation);
-
-                        // Rule 4: Check if a team is playing against itself.
-                        if (homeClub != awayClub)
-                        {
-                            // Update club statistics based on match result.
-                            homeClub.GamesPlayed++;
-                            awayClub.GamesPlayed++;
-
-                            homeClub.GoalsFor += homeGoals;
-                            homeClub.GoalsAgainst += awayGoals;
-                            awayClub.GoalsFor += awayGoals;
-                            awayClub.GoalsAgainst += homeGoals;
-
-                            if (homeGoals > awayGoals)
-                            {
-                                homeClub.GamesWon++;
-                                awayClub.GamesLost++;
-                            }
-                            else if (homeGoals < awayGoals)
-                            {
-                                homeClub.GamesLost++;
-                                awayClub.GamesWon++;
-                            }
-                            else
-                            {
-                                homeClub.GamesDrawn++;
-                                awayClub.GamesDrawn++;
-                            }
-                        }
-                        else
-                        {
-                            // Rule 5: Teams cannot play against themselves.
-                            Console.WriteLine($"Error in round-{roundNumber + 1}. " +
-                                              $"{homeTeamAbbreviation} vs. {awayTeamAbbreviation}: " +
-                                              "A team cannot play against itself.");
-                            return; // Stop processing further rounds.
-                        }
-                    }
+                    homeClub.GamesWon++;
+                    awayClub.GamesLost++;
                 }
-                catch (Exception ex)
+                else if (homeGoals < awayGoals)
                 {
-                    // Rule 1: Error handling
-                    Console.WriteLine($"Error in round-{roundNumber + 1}. {ex.Message}");
-                    return; // Stop processing further rounds.
+                    homeClub.GamesLost++;
+                    awayClub.GamesWon++;
                 }
+                else
+                {
+                    homeClub.GamesDrawn++;
+                    awayClub.GamesDrawn++;
+                }
+            }
+
+            // Calculate winning streaks after each round (up to 5 latest played games).
+            foreach (var club in clubs)
+            {
+                club.WinningStreak = CalculateWinningStreak(rounds, club.Abbreviation);
             }
         }
 
@@ -98,7 +67,7 @@ class Program
                      .ThenByDescending(c => c.GoalsFor)
                      .ToList();
 
-        // Display the current standings in console. Formatting
+        // Display the current standings.
         Console.WriteLine($"{"Pos",4} {"Special",8} {"Club Name",-25} {"GP",3} {"W",3} {"D",3} {"L",3} " +
                           $"{"GF",3} {"GA",3} {"GD",3} {"Pts",3} {"Winning Streak",12}");
         Console.WriteLine(new string('-', 80));
@@ -137,17 +106,43 @@ class Program
         return rounds;
     }
 
-    // Display the current standings for a list of clubs.
-    static void DisplayTable(List<FootballClub> clubs)
+    // Calculate the winning streak for a club based on match data.
+    static string CalculateWinningStreak(List<List<string>> rounds, string clubAbbreviation)
     {
-        // Display the table for the given list of clubs.
-        Console.WriteLine($"{"Pos",4} {"Special",8} {"Club Name",-25} {"GP",3} {"W",3} {"D",3} {"L",3} " +
-                          $"{"GF",3} {"GA",3} {"GD",3} {"Pts",3} {"Winning Streak",12}");
-        Console.WriteLine(new string('-', 80));
-
-        for (int i = 0; i < clubs.Count; i++)
+        var streak = new List<string>();
+        for (int i = rounds.Count - 1; i >= 0 && streak.Count < 5; i--)
         {
-            Console.WriteLine($"{i + 1,4}. {clubs[i]}");
+            var roundMatches = rounds[i];
+            foreach (var match in roundMatches)
+            {
+                var matchData = match.Split(',');
+                string homeTeamAbbreviation = matchData[0];
+                string awayTeamAbbreviation = matchData[1];
+                int homeGoals = int.Parse(matchData[2].Split('-')[0]);
+                int awayGoals = int.Parse(matchData[2].Split('-')[1]);
+
+                if (clubAbbreviation == homeTeamAbbreviation)
+                {
+                    if (homeGoals > awayGoals)
+                        streak.Add("W");
+                    else if (homeGoals < awayGoals)
+                        streak.Add("L");
+                    else
+                        streak.Add("D");
+                }
+                else if (clubAbbreviation == awayTeamAbbreviation)
+                {
+                    if (awayGoals > homeGoals)
+                        streak.Add("W");
+                    else if (awayGoals < homeGoals)
+                        streak.Add("L");
+                    else
+                        streak.Add("D");
+                }
+            }
         }
+
+        streak.Reverse();
+        return string.Join("|", streak);
     }
 }
