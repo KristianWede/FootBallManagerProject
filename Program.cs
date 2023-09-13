5f44ecb2d5a3e2b082834e75cb7a50a7728fc1b9
KristianWede <kris79s4@stud.kea.dk>
2023-09-13 13:31:43 +0200

    Added 3 rules so far;

--- Program.cs
+++ Program.cs
@@ -7,11 +7,22 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        Program prg = new Program();
+
+        prg.InitLoad();
+
+    }
+
+    public void InitLoad()
+    {
         // Load setup, teams, and round data into appropriate data structures.
         // Assuming you have already loaded the data into the following lists.
+        
         List<FootballClub> clubs = LoadTeams("./csvfiler/teams.csv");
         List<List<string>> rounds = LoadRounds(); // Load round data into a list of lists.
 
+        try{
         // Process each round to update club statistics.
         foreach (var round in rounds)
         {
@@ -21,12 +32,25 @@ class Program
                 var matchData = match.Split(',');
                 string homeTeamAbbreviation = matchData[0];
                 string awayTeamAbbreviation = matchData[1];
+
+                //Rule 4
+                if(homeTeamAbbreviation.Equals(awayTeamAbbreviation)){
+                    throw new Exception("The same team cannot play against each other.");
+                }
+
                 int homeGoals = int.Parse(matchData[2].Split('-')[0]);
                 int awayGoals = int.Parse(matchData[2].Split('-')[1]);
 
                 // Find the corresponding home and away clubs.
                 var homeClub = clubs.First(c => c.Abbreviation == homeTeamAbbreviation);
                 var awayClub = clubs.First(c => c.Abbreviation == awayTeamAbbreviation);
+
+                //Checks if the teams inside the "rounds" exists inside the teams.csv //Rule 2
+                bool exists = round.Any(innerList => innerList.Contains(awayClub.FullClubName) || innerList.Contains(homeClub.FullClubName));
+
+                if(exists){
+                    continue;
+                }
 
                 // Update club statistics based on match result.
                 homeClub.GamesPlayed++;
@@ -67,6 +91,18 @@ class Program
                      .ThenByDescending(c => c.GoalsFor)
                      .ToList();
 
+        //lowerFraction
+        
+
+        //upperFraction
+
+        }catch(Exception){
+        Console.WriteLine("Something went wrong in while processing teams!");
+        Environment.Exit(1); //Stops the program
+        }
+
+        try
+        {
         // Display the current standings.
         Console.WriteLine($"{"Pos",4} {"Special",8} {"Club Name",-25} {"GP",3} {"W",3} {"D",3} {"L",3} " +
                           $"{"GF",3} {"GA",3} {"GD",3} {"Pts",3} {"Winning Streak",12}");
@@ -76,11 +112,20 @@ class Program
         {
             Console.WriteLine($"{i + 1,4}. {clubs[i]}");
         }
-    }
+        }catch(Exception){
+        Console.WriteLine("Something went wrong in while displaying teams!");
+        Environment.Exit(1); //Stops the program
+        }
+
+
+
+    }   
 
     // Load team data from the provided CSV file.
-    static List<FootballClub> LoadTeams(string fileName)
+    public List<FootballClub> LoadTeams(string fileName)
     {
+        try{
+
         var teams = new List<FootballClub>();
         foreach (var line in File.ReadLines(fileName).Skip(1)) // Skip the header line
         {
@@ -88,27 +133,44 @@ class Program
             teams.Add(club);
         }
         return teams;
+        
+        }catch(Exception){
+            Console.WriteLine("Something went wrong in LoadTeams!");
+            Environment.Exit(1); //Stops the program
+            return null; //To prevent compile error
+        }
     }
 
     // Load match data from round files and return them as a list of lists.
-    static List<List<string>> LoadRounds()
+    public List<List<string>> LoadRounds()
     {
-        var rounds = new List<List<string>>();
-        for (int roundNumber = 1; roundNumber <= 32; roundNumber++)
-        {
-            string fileName = $"round-{roundNumber}.csv";
-            if (File.Exists(fileName))
+
+        try{
+
+            var rounds = new List<List<string>>();
+            for (int roundNumber = 1; roundNumber <= 32; roundNumber++)
             {
-                var roundMatches = File.ReadAllLines(fileName).Skip(1).ToList();
-                rounds.Add(roundMatches);
+                string fileName = $"round-{roundNumber}.csv";
+                if (File.Exists(fileName))
+                {
+                    var roundMatches = File.ReadAllLines(fileName).Skip(1).ToList();
+                    rounds.Add(roundMatches);
+                }
             }
+            return rounds;
+
+            }catch(Exception){
+            Console.WriteLine("Something went wrong in LoadRounds!");
+            Environment.Exit(1); //Stops the program
+            return null; //To prevent compile error
         }
-        return rounds;
     }
 
     // Calculate the winning streak for a club based on match data.
-    static string CalculateWinningStreak(List<List<string>> rounds, string clubAbbreviation)
+    public string CalculateWinningStreak(List<List<string>> rounds, string clubAbbreviation)
     {
+        try{
+
         var streak = new List<string>();
         for (int i = rounds.Count - 1; i >= 0 && streak.Count < 5; i--)
         {
@@ -144,5 +206,13 @@ class Program
 
         streak.Reverse();
         return string.Join("|", streak);
+
+        }catch(Exception){
+        Console.WriteLine("Something went wrong in CalculateWinningStreak!");
+        Environment.Exit(1); //Stops the program
+        return null; //To prevent compile error            }
+    }
+
     }
 }
+
